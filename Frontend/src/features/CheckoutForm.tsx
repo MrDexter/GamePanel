7feccfb6ee4783c7f5aac801b3ac5f3b56ff2a68
MrDexter <declanpage100@gmail.com>
2026-05-03 23:12:07 +0100

    Update CheckoutForm.tsx

--- Frontend/src/features/CheckoutForm.tsx
+++ Frontend/src/features/CheckoutForm.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import {useCheckoutElements} from '@stripe/react-stripe-js/checkout';
+// import React from 'react';
+// import {useCheckoutElements} from '@stripe/react-stripe-js/checkout';
 
-const CheckoutForm = () => {
-  const checkoutState = useCheckoutElements();
+// const CheckoutForm = () => {
+//   const checkoutState = useCheckoutElements();
 
-  if (checkoutState.type === 'loading') {
-    return (
-      <div>Loading...</div>
-    );
-  }
+//   if (checkoutState.type === 'loading') {
+//     return (
+//       <div>Loading...</div>
+//     );
+//   }
 
-  if (checkoutState.type === 'error') {
-    return (
-      <div>Error: {checkoutState.error.message}</div>
-    );
-  }
+//   if (checkoutState.type === 'error') {
+//     return (
+//       <div>Error: {checkoutState.error.message}</div>
+//     );
+//   }
 
-  return (
-    <form>
-      {JSON.stringify(checkoutState.checkout.lineItems, null, 2)}
-      {/* A formatted total amount */}
-      Total: {checkoutState.checkout.total.total.amount}
-    </form>
-  );
-};
+//   return (
+//     <form>
+//       {JSON.stringify(checkoutState.checkout.lineItems, null, 2)}
+//       {/* A formatted total amount */}
+//       Total: {checkoutState.checkout.total.total.amount}
+//     </form>
+//   );
+// };
